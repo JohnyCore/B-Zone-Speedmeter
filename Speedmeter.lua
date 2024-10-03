c027d45fab0f3d4739c87fc5855db3b5538332b6
DjC <37981460+JohnyCore@users.noreply.github.com>
2024-10-03 20:11:11 +0330

    Update - Advanced Conditional Checks

--- Speedmeter.lua
+++ Speedmeter.lua
@@ -1,14 +1,17 @@
 local script_name = "Speedmeter for B-Zone"
-local script_version = "1.0.1"
+local script_version = "1.0.2"
 local script_author = "DjC"
 
 local samp = require "samp.events"
 local sampev = require "lib.samp.events"
 require "lib.moonloader"
 
-local speedTextdrawId1 = nil
-local speedTextdrawId2 = nil
-local unitTextdrawId2 = nil
+local speedTextdraws = {
+    speedTextdrawId1 = {text = "Speed: 0 Km/h", formatString = "Speed: %.0f Km/h"},
+    speedTextdrawId01 = {text = "Viteza: 0 Km/h", formatString = "Viteza: %.0f Km/h"},
+    speedTextdrawId2 = {text = "0", formatString = "%.0f", colorThreshold = {100, 150}},
+    unitTextdrawId2 = {text = "KM/H"}
+}
 
 function main()
     if not isSampfuncsLoaded() or not isSampLoaded() then return end
@@ -18,87 +21,55 @@ function main()
 
     while true do
         wait(0)
-        handleSpeedTextdraw1(scalingFactor)
+        handleSpeedTextdraw('speedTextdrawId1', scalingFactor)
+        handleSpeedTextdraw('speedTextdrawId01', scalingFactor)
         handleSpeedTextdraw2(scalingFactor)
-        handleSpeedTextdraw01(scalingFactor)
     end
 end
 
-function handleSpeedTextdraw1(scalingFactor)
+function handleSpeedTextdraw(idVar, scalingFactor)
     if isCharInAnyCar(playerPed) then
-        if not speedTextdrawId1 then
-            speedTextdrawId1 = getTextdrawIdByText("Speed: 0 Km/h") 
-            if speedTextdrawId1 then
-                -- sampAddChatMessage("Speed textdraw 1 ID found: " .. speedTextdrawId1, -1)
-            else
-                -- sampAddChatMessage("Speed textdraw 1 not found. Please check your textdraws.", -1)
-            end
+        if not speedTextdraws[idVar].id then
+            speedTextdraws[idVar].id = getTextdrawIdByText(speedTextdraws[idVar].text)
         end
-        if speedTextdrawId1 then
+        if speedTextdraws[idVar].id then
             local car = storeCarCharIsInNoSave(playerPed)
-            local speed = getCarSpeed(car) * 3.6 * scalingFactor 
-            sampTextdrawSetString(speedTextdrawId1, string.format("Speed: %.0f Km/h", speed))
+            local speed = getCarSpeed(car) * 3.6 * scalingFactor
+            sampTextdrawSetString(speedTextdraws[idVar].id, string.format(speedTextdraws[idVar].formatString, speed))
         end
     else
-        speedTextdrawId1 = nil
-    end
-end
-
-function handleSpeedTextdraw01(scalingFactor)
-    if isCharInAnyCar(playerPed) then
-        if not speedTextdrawId01 then
-            speedTextdrawId01 = getTextdrawIdByText("Viteza: 0 Km/h") 
-            if speedTextdrawId01 then
-                -- sampAddChatMessage("Speed textdraw 1 ID found: " .. speedTextdrawId01, -1)
-            else
-                -- sampAddChatMessage("Speed textdraw 1 not found. Please check your textdraws.", -1)
-            end
-        end
-        if speedTextdrawId01 then
-            local car = storeCarCharIsInNoSave(playerPed)
-            local speed = getCarSpeed(car) * 3.6 * scalingFactor 
-            sampTextdrawSetString(speedTextdrawId01, string.format("Viteza: %.0f Km/h", speed))
-        end
-    else
-        speedTextdrawId01 = nil
+        speedTextdraws[idVar].id = nil
     end
 end
 
 function handleSpeedTextdraw2(scalingFactor)
     if isCharInAnyCar(playerPed) then    
-        if not speedTextdrawId2 or not unitTextdrawId2 then
-            speedTextdrawId2 = getTextdrawIdByText("0")
-            unitTextdrawId2 = getTextdrawIdByText("KM/H")
-            if speedTextdrawId2 and unitTextdrawId2 then
-                -- sampAddChatMessage("Speed textdraw 2 ID: " .. speedTextdrawId2 .. " | Unit textdraw ID: " .. unitTextdrawId2, -1)
-            else
-                if not speedTextdrawId2 then
-                    -- sampAddChatMessage("Speed textdraw 2 '0' not found.", -1)
-                end
-                if not unitTextdrawId2 then
-                    -- sampAddChatMessage("Unit textdraw 'KM/H' not found.", -1)
-                end
-            end
+        if not speedTextdraws.speedTextdrawId2.id or not speedTextdraws.unitTextdrawId2.id then
+            speedTextdraws.speedTextdrawId2.id = getTextdrawIdByText(speedTextdraws.speedTextdrawId2.text)
+            speedTextdraws.unitTextdrawId2.id = getTextdrawIdByText(speedTextdraws.unitTextdrawId2.text)
         end
-        if speedTextdrawId2 then
+
+        if speedTextdraws.speedTextdrawId2.id then
             local car = storeCarCharIsInNoSave(playerPed)
             local speed = getCarSpeed(car) * 3.6 * scalingFactor
-            if speed < 100 then
-                sampTextdrawSetString(speedTextdrawId2, string.format("%.0f", speed))
-            elseif speed >= 100 and speed < 150 then
-                sampTextdrawSetString(speedTextdrawId2, string.format("~y~%.0f", speed))
-            elseif speed >= 150 then
-                sampTextdrawSetString(speedTextdrawId2, string.format("~r~%.0f", speed))
+            local formattedSpeed = string.format(speedTextdraws.speedTextdrawId2.formatString, speed)
+
+            if speed >= speedTextdraws.speedTextdrawId2.colorThreshold[1] and speed < speedTextdraws.speedTextdrawId2.colorThreshold[2] then
+                formattedSpeed = "~y~" .. formattedSpeed
+            elseif speed >= speedTextdraws.speedTextdrawId2.colorThreshold[2] then
+                formattedSpeed = "~r~" .. formattedSpeed
             end
+
+            sampTextdrawSetString(speedTextdraws.speedTextdrawId2.id, formattedSpeed)
         end
     else
-        speedTextdrawId2 = nil
-        unitTextdrawId2 = nil
+        speedTextdraws.speedTextdrawId2.id = nil
+        speedTextdraws.unitTextdrawId2.id = nil
     end
 end
 
 function getTextdrawIdByText(targetText)
-    for i = 2000, 2150 do
+    for i = 0, 2150 do
         local text = sampTextdrawGetString(i)
         if text and text == targetText then
             return i
